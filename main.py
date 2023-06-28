50ebe67d30d36ae51d0b54963143a61bd582c4d6
m1jlariosm <jululama1@gmail.com>
2023-06-28 09:51:25 -0600

    Add images and descriptions

--- main.py
+++ main.py
@@ -34,7 +34,11 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
